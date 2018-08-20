aa7bae7d7d1ff28ee6d30d2c347844164c392fea
Lixiang Zhang <lz3100@att.com>
2018-08-19 20:42:21 -0700

    Add quick return feature

--- app/src/main/java/com/example/gavinzhang/simpleflickrapp/ui/master/ui/MasterFragment.kt
+++ app/src/main/java/com/example/gavinzhang/simpleflickrapp/ui/master/ui/MasterFragment.kt
@@ -1,5 +1,6 @@
 package com.example.gavinzhang.simpleflickrapp.ui.master.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -17,6 +18,9 @@ import com.example.gavinzhang.simpleflickrapp.ui.master.adapters.FlickrAdapter
 import com.example.gavinzhang.simpleflickrapp.ui.master.data.FlickrRepository
 import com.example.gavinzhang.simpleflickrapp.ui.master.utils.NetworkStatus
 import com.example.gavinzhang.simpleflickrapp.ui.master.viewmodels.SharedViewModel
+import com.felipecsl.quickreturn.library.QuickReturnAttacher
+import com.felipecsl.quickreturn.library.widget.QuickReturnAdapter
+import com.felipecsl.quickreturn.library.widget.QuickReturnTargetView
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
@@ -28,11 +32,13 @@ class MasterFragment : DaggerFragment() {
     lateinit var listview: ListView
     lateinit var swipeRefreshLayout: SwipeRefreshLayout
     lateinit var imageTitle: TextView
+    lateinit var quickReturn: TextView
     private lateinit var viewModel: SharedViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         val view = inflater.inflate(R.layout.master_fragment, container, false)
+        quickReturn = view.findViewById(R.id.quick_return)
         imageTitle = view.findViewById(R.id.imageTitle)
         listview = view.findViewById(R.id.listview)
         swipeRefreshLayout = view.findViewById(R.id.swipeRefreshLayout)
@@ -67,13 +73,15 @@ class MasterFragment : DaggerFragment() {
     private fun observeItems() {
         viewModel.itemsLiveData?.observe(this, Observer {
             val adapter = FlickrAdapter(context, it)
-            listview.adapter = adapter
-            listview.setOnItemClickListener { _, view, position, l ->
+            listview.adapter = QuickReturnAdapter(adapter)
+            val attacher = QuickReturnAttacher.forView(listview)
+            attacher.addTargetView(quickReturn, QuickReturnTargetView.POSITION_TOP, dpToPx(activity!!, 50f))
+            listview.setOnItemClickListener { _, view, position, _ ->
                 val action = MasterFragmentDirections
                         .actionMasterFragmentToDetailFragment()
                 action.run {
-                    setUrl(adapter.getUrl(it[position]))
-                    setTitle(it[position].title)
+                    setUrl(adapter.getUrl(it[position - 1]))
+                    setTitle(it[position - 1].title)
                 }
                 Navigation.findNavController(view).navigate(action)
             }
@@ -96,5 +104,10 @@ class MasterFragment : DaggerFragment() {
         viewModel.titleLiveData.observe(this, Observer {
             imageTitle.text = it
         })
+    }
+
+    private fun dpToPx(context: Context, dp: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (dp * scale + 0.5f).toInt()
     }
 }
